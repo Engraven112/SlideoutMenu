e05ee7f4b68507fecce48439952e659aa8bcad22
Sopheap Hok <sopheap.hok@rebel.com>
2018-02-13 09:01:24 -0500

    adding boxes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,7 +5,12 @@ export default class Home extends React.Component {
     render() {
         return (
             <div className="container">
-                <h1>Hello world</h1>
+                <div className="boxes">
+                    <div className="box" />
+                    <div className="box" />
+                    <div className="box" />
+                    <div className="box" />
+                </div>
             </div>
         );
     }
